589f84760c81e2fd3b716af0d286f074ba8c35b0
Zach Cohn <cohn.zach@gmail.com>
2023-05-14 06:27:30 +0000

    Add routes for home, register, login

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,11 +1,30 @@
-import Register from './Register'
+import Register from './pages//Register'
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
 
   return (
-    <main className="App">
-      <Register />
-    </main>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Log In</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route  path="/" element={<Home />} />
+        <Route path="/register" element={<Register />}/>
+        <Route path="*" element={<h1>Page Not Found</h1>}></Route>
+      </Routes>
+    </>
   )
 }
 
