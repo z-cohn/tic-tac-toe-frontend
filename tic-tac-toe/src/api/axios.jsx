dc40e5d43d8d5b4a67942c727a00d963fa9776f6
Zach Cohn <cohn.zach@gmail.com>
2023-06-04 13:03:07 +0000

    Implement basic login, jwt, and jwt refresh functionality, and basic user info page as a demo

--- tic-tac-toe/src/api/axios.jsx
+++ tic-tac-toe/src/api/axios.jsx
@@ -1,5 +1,12 @@
 import axios from 'axios'
+const BASE_URL = 'http://localhost:4001';
 
 export default axios.create({
-    baseURL: 'http://localhost:4001'
+    baseURL: BASE_URL
+})
+
+export const axiosPrivate = axios.create({
+    baseURL: BASE_URL,
+    headers: { 'Content-Type': 'application/json'/*, Authorization: "Bearer " + auth?.accessToken*/ },
+    withCredentials: true
 })
--- tic-tac-toe/src/pages/Home.jsx
+++ tic-tac-toe/src/pages/Home.jsx
@@ -1,6 +1,8 @@
+import Hero from "../components/Hero";
+
 function Home() {
     return (
-        <h1>Homepage</h1>
+        <Hero />
     )
 }
 
