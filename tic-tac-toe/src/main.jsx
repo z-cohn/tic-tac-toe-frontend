61f25c75b21cb25f6155b151a2a43ecf883c509b
Zach Cohn <cohn.zach@gmail.com>
2023-06-09 13:52:30 +0000

    Move css files to styles dir

--- tic-tac-toe/src/main.jsx
+++ tic-tac-toe/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
+import './styles/index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthProvider.jsx'
 
